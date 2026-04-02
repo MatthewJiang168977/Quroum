9cd9abe1920c0768b72200271a848d8d28ba3c69
Matthew Jiang <mmj9715@nyu.edu>
2026-04-02 16:01:58 -0400

    fixed CORs for frontend connections

--- src/server.js
+++ src/server.js
@@ -23,7 +23,7 @@ connectDB();
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || "http://localhost:3000" }));
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json({ limit: "50mb" }));
 
